f32b30f1f0bdcc36550e0d137da732883a7e016e
Alexsa7161 <73057920+Alexsa7161@users.noreply.github.com>
2023-03-27 22:17:28 +0300

    Delete task1.sql

--- task1.sql
+++
@@ -1,8 +0,0 @@
-/*
-Создайте отчёт с отступом, в котором отражается иерархия управления, начиная с сотрудника по фамилии Kochhar.
-Выведите фамилии, номера менеджеров и номера отделов сотрудников.
-*/
-SELECT LPAD(LAST_NAME, LENGTH(LAST_NAME) + LEVEL*3-3, '_') NAME, MANAGER_ID MANAGER, DEPARTMENT_ID DEPTNO
-FROM EMPLOYEES
-START WITH LAST_NAME = 'Kochhar'
-CONNECT BY PRIOR EMPLOYEE_ID = MANAGER_ID;
