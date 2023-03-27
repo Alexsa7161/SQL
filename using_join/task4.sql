4977e78d735478ef464a3d1fbebaaaf30105f063
Alexsa7161 <73057920+Alexsa7161@users.noreply.github.com>
2023-03-27 20:54:56 +0300

    Update task4.sql

--- using_join/task4.sql
+++ using_join/task4.sql
@@ -1,5 +1,5 @@
 /*
-Выведите фамилии и номера всех служащих вместе с фамилиями и номерами их менеджеров.
+Р’С‹РІРµРґРёС‚Рµ С„Р°РјРёР»РёРё Рё РЅРѕРјРµСЂР° РІСЃРµС… СЃР»СѓР¶Р°С‰РёС… РІРјРµСЃС‚Рµ СЃ С„Р°РјРёР»РёСЏРјРё Рё РЅРѕРјРµСЂР°РјРё РёС… РјРµРЅРµРґР¶РµСЂРѕРІ.
 */
 SELECT EMP1.LAST_NAME "Employee", EMP1.EMPLOYEE_ID "Emp#", EMP2.LAST_NAME "Manager", EMP2.EMPLOYEE_ID "Mgr#"
 FROM EMPLOYEES EMP1 LEFT JOIN EMPLOYEES EMP2 ON EMP2.EMPLOYEE_ID=EMP1.MANAGER_ID;
