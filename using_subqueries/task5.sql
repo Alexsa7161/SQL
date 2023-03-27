66bf640a037243e617dffbba0b87515ecf1905bd
Alexsa7161 <73057920+Alexsa7161@users.noreply.github.com>
2023-03-27 21:01:37 +0300

    Add files via upload

---
+++ using_subqueries/task5.sql
@@ -0,0 +1,11 @@
+/*
+Выведите номера, фамилии и наименования отделов всех сотрудников.
+Используйте скалярный подзапрос в команде SELECT для вывода наименований отделов.
+*/
+SELECT EMPLOYEE_ID, LAST_NAME,
+(SELECT DEPARTMENT_NAME
+FROM DEPARTMENTS D
+WHERE E.DEPARTMENT_ID =
+D.DEPARTMENT_ID ) DEPARTMENT
+FROM EMPLOYEES E
+ORDER BY DEPARTMENT;
