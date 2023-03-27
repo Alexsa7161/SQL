6c1934ff880bc51b3681fa10fc8fad1a0b1c4e28
Alexsa7161 <73057920+Alexsa7161@users.noreply.github.com>
2023-03-27 19:55:09 +0300

    Add files via upload

---
+++ unusual_queries/recursive_with.sql
@@ -0,0 +1,15 @@
+/*
+Создать запрос, который выведет из символьного столбца таблицы всю информацию за исключением значений,
+которые могут быть получены из другого  значения столбца за счет циклической перестановки символов.
+*/
+with doptable as (select text,length(text),DENSE_RANK() over(ORDER BY TEXT) RES from Input),
+RESULT (TEXT, N, R,RES) AS
+(SELECT TEXT, 0 N, '' R, RES
+FROM DOPTABLE
+UNION ALL
+SELECT TEXT, N + 1, SUBSTR(RESULT.TEXT,N+1,LENGTH(TEXT))|| SUBSTR(RESULT.TEXT, 1, N), RES
+FROM RESULT
+WHERE N+2 < LENGTH(TEXT))
+SELECT TEXT FROM TAB13 MINUS SELECT DISTINCT TEXT FROM RESULT WHERE RES IN (
+SELECT RES FROM 
+( SELECT RES,R FROM RESULT WHERE R IN  (SELECT R FROM RESULT WHERE R IS NOT NULL GROUP BY R HAVING COUNT(R)>1)));
---
+++ unusual_queries/using_connect_by.sql
@@ -0,0 +1,53 @@
+/*
+Проверить наличие циклов в таблице подчиненностей.
+Вывести циклические зависимости в строчку в виде Номер1.Имя1->Номер2.Имя2->…Номер1.Имя1, начиная с первого по алфавиту имени.
+*/
+WITH TAB AS( 
+        SELECT '1' NUM, 'Алексей' NAME, '2' MNG 
+        FROM DUAL 
+        UNION ALL 
+        SELECT '2' NUM, 'Петр' NAME, '3' MNG 
+        FROM DUAL 
+        UNION ALL 
+        SELECT '3' NUM, 'Павел' NAME, '4' MNG 
+        FROM DUAL 
+        UNION ALL 
+        SELECT '4' NUM, 'Иван' NAME, '2' MNG 
+        FROM DUAL 
+        UNION ALL 
+        SELECT '5' NUM, 'Кристина' NAME, '3' MNG 
+        FROM DUAL 
+        UNION ALL 
+        SELECT '6' NUM, 'Андрей' NAME, '5' MNG 
+        FROM DUAL
+        UNION ALL 
+        SELECT '7' NUM, 'Петр' NAME, '8' MNG 
+        FROM DUAL 
+        UNION ALL 
+        SELECT '8' NUM, 'Павел' NAME, '9' MNG 
+        FROM DUAL 
+        UNION ALL 
+        SELECT '9' NUM, 'Иван' NAME, '7' MNG 
+        FROM DUAL 
+        ), 
+     RES AS (
+        SELECT LTRIM(SYS_CONNECT_BY_PATH(NUM||'.'||NAME, '->')||'->'||connect_by_root(NUM||'.'||name),'->') A, SYS_CONNECT_BY_PATH(NUM,'.') B
+        FROM TAB 
+        WHERE connect_by_root(num) = mng 
+        CONNECT BY NOCYCLE PRIOR mng = num 
+        ORDER BY connect_by_root(NAME)
+        ), 
+     ORD AS (
+        SELECT A, REGEXP_REPLACE(B,B) || (SELECT REGEXP_REPLACE(B,B) || LISTAGG(N,'.') WITHIN GROUP (ORDER BY N) 
+                   FROM (SELECT REGEXP_SUBSTR(B,'\\d+',1,LEVEL) N, LEVEL LV 
+                         FROM DUAL 
+                         CONNECT BY REGEXP_SUBSTR(B,'\\d+',1,LEVEL) IS NOT NULL)) B, ROWNUM RNN 
+        FROM RES
+     ),
+     GR AS (
+        SELECT ROW_NUMBER() OVER (PARTITION BY B ORDER BY RNN) RN, A, B
+        FROM ORD
+     )
+SELECT A RESULT 
+FROM GR 
+WHERE RN = 1;
